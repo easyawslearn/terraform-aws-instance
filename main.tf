a26cbe5ef89cb7ec50451051bc54a37f4ada7811
vijay.patel <to.patelvijay@gmail.com>
2019-02-23 21:20:33 +0530

    commented extra code where given example for module reference for security group

--- main.tf
+++ main.tf
@@ -24,10 +24,13 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+/*
+// Use Security Group module to create security group
 module "security_group" {
   source = "git::https://github.com/Patelvijaykumar/terraform-aws-security-group"
   vpc_id = "${var.vpc_id}"
 }
+*/
 
 resource "aws_security_group" "allow_all" {
   count       = "${var.iscreate == "" ? 1 : 0}"
